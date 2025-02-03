f1e392c7d61264f87e789ef6578786c47d13aee1
hasanyelaldi <hasanyelaldi9@gmail.com>
2025-02-03 14:29:53 +0300

    feat: more configurations have been added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkblue: '#161664',
+        midblue: '#3636a6',
+        lightblue: '#796dd5',
+      }
+    }
   },
   plugins: [],
 }
